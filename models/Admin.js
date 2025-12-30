4140a01b3b4be99c68a8a55f502f5123c20606e5
ZdapX <stokmlmurmer150@gmail.com>
2025-12-30 09:57:58 +0700

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -2,31 +2,12 @@
 const mongoose = require('mongoose');
 
 const AdminSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    pass: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    quote: {
-        type: String,
-        default: ''
-    },
-    hashtag: {
-        type: String, // Contoh: #webdev #backend
-        default: ''
-    },
-    profilePic: {
-        type: String, // URL Foto Profile dari Cloudinary
-        default: 'https://via.placeholder.com/150'
-    }
+    username: { type: String, required: true, unique: true },
+    pass: { type: String, required: true },
+    name: { type: String, required: true },
+    quote: { type: String, default: '' },
+    hashtag: { type: String, default: '' },
+    profilePic: { type: String, default: '' }
 });
 
-module.exports = mongoose.model('Admin', AdminSchema);
+module.exports = mongoose.models.Admin || mongoose.model('Admin', AdminSchema);
