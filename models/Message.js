7e2cf242fd927aa5348fb358ffc37cf5bb23f37e
ZdapX <stokmlmurmer150@gmail.com>
2025-12-30 12:28:53 +0700

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -5,6 +5,7 @@ const MessageSchema = new mongoose.Schema({
     user: { type: String, required: true },
     text: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
+    to: { type: String, required: true }, // Ditujukan ke: 'Silverhold' atau 'BraynOfficial'
     timestamp: { type: Date, default: Date.now }
 });
 
