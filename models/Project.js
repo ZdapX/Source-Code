f0e632b91d4e8ea4c2e50ca84bd4200aeddd3b22
ZdapX <stokmlmurmer150@gmail.com>
2026-01-01 11:39:17 +0700

    Update Project.js

--- models/Project.js
+++ models/Project.js
@@ -5,9 +5,15 @@ const ProjectSchema = new mongoose.Schema({
     name: { type: String, required: true },
     language: { type: String, required: true },
     type: { type: String, enum: ['file', 'code'], required: true },
-    content: { type: String, required: true },
+    content: { type: String }, // Digunakan untuk teks kodingan (Paste Code)
+    
+    // Field baru untuk menyimpan file langsung di MongoDB
+    fileData: { type: Buffer }, 
+    fileType: { type: String }, // Contoh: application/zip
+    fileName: { type: String }, // Contoh: projek_saya.zip
+    
     note: { type: String, default: '' },
-    preview: { type: String, default: '' },
+    preview: { type: String, default: '' }, // Preview gambar tetap di Cloudinary (karena gambar jarang error 401)
     likes: { type: Number, default: 0 },
     downloads: { type: Number, default: 0 },
     uploadedBy: { type: String, required: true },
